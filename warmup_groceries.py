6194c0b3009691bd5fcd21cfa702775cef702a8a
yuchienchen <yuchienc@yahoo.com>
2024-07-30 18:19:27 -0500

    add 'target' empty dict

--- warmup_groceries.py
+++ warmup_groceries.py
@@ -5,12 +5,28 @@ def main():
     'costco': {'croissant': 12}
     }
 
-    groceries['target'] = {'sugar cookies': 1}
-    print(groceries)
-    print()
+    groceries['target'] = {}
 
-    groceries['safeway'] = {'eggs': 3, 'coconut milk': 3, 'flour': 1}
+    for key, value in groceries.items():
+        print(key)
+        print(value)
     print(groceries)
+
+        # sub_dict = value
+        # for key, value in sub_dict.items():
+        #     print(key)
+        #     print(value)
+        #     target['sugar cookies'] = 1
+        # print(sub_dict)
+    
+
+
+    # groceries['target'] = {'sugar cookies': 1}
+    # print(groceries)
+    # print()
+
+    # groceries['safeway'] = {'eggs': 3, 'coconut milk': 3, 'flour': 1}
+    # print(groceries)
 
 
 
