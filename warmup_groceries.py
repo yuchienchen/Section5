a18979c9078d6ed9aff052955624b8e08594a1d4
yuchienchen <yuchienc@yahoo.com>
2024-07-30 17:34:17 -0500

    seems like still not right though got the right print

---
+++ warmup_groceries.py
@@ -0,0 +1,18 @@
+def main():
+
+    groceries = {
+    'safeway': {'eggs': 1, 'coconut milk': 3}, 
+    'costco': {'croissant': 12}
+    }
+
+    groceries['target'] = {'sugar cookies',1}
+    print(groceries)
+    print()
+
+    groceries['safeway'] = {'eggs': 3, 'coconut milk': 3, 'flour': 1}
+    print(groceries)
+
+
+
+if __name__ == '__main__':
+    main()
